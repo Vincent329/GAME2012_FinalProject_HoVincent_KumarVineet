3cf5c25263086857d27fb557cc33aeb791d9e30b
Vineet Kumar <vineet.z91@gmail.com>
2020-12-13 18:12:38 -0500

    Back Wall put into place.

--- GAME2012_FinalProject/FirstExample/GAME2012_FinalProject_HoVincent_KumarVineet.cpp
+++ GAME2012_FinalProject/FirstExample/GAME2012_FinalProject_HoVincent_KumarVineet.cpp
@@ -574,7 +574,28 @@ void init(void)
 	placeCylinder(glm::vec3(19.25f, 0.0f, -26.75f), 12, 5, castleWallsTx, { 2.0f,1.0f,2.0f, }, { 1.0f,1.0f,1.0f }, { 0,1,0 }, 0);
 	placeCone(glm::vec3(18.75f, 5.0f, -27.5f), 10, 3, castleWallsTx, { 3.0f,1.0f,3.0f }, { 1.0f,1.0f,1.0f }, { 0,1,0 }, 0);
 
-	//
+	
+	//Back Wall ----
+	placeCube(glm::vec3(-1.5f, 0.0f, 0.25f), 1.25f, 22.0f, 4.0f, castleWallsTx, 1.0f);
+	//placeCube(glm::vec3(19.0f, 4.0f, -25.75f), 26.0f, 0.5f, 0.25f, castleWallsTx, 1.0f);
+	//placeCube(glm::vec3(21.0f, 4.0f, -25.75f), 26.0f, 0.5f, 0.25f, castleWallsTx, 1.0f);
+
+	// Battlements - merlons and crenels
+	// Back
+	/*float merlon_3 = -0.75f;
+	for (int i = 0; i < 17; i++, merlon_2 -= 1.5f)
+	{
+		if (i == 16 || i == 0)
+		{
+			placeCube(glm::vec3(19.0f, 4.25f, merlon_3), 1.5f, 0.5f, 0.75f, castleWallsTx, 1.0f);
+			placeCube(glm::vec3(21.0f, 4.25f, merlon_3), 1.5f, 0.5f, 0.75f, castleWallsTx, 1.0f);
+		}
+		else
+		{
+			placeCube(glm::vec3(19.0f, 4.25f, merlon_3), 1.250f, 0.5f, 0.75f, castleWallsTx, 1.0f);
+			placeCube(glm::vec3(21.0f, 4.25f, merlon_3), 1.250f, 0.5f, 0.75f, castleWallsTx, 1.0f);
+		}
+	}*/
 
 	// ------------------------- Vince -----------------
 	// ------------------------- Back Wall and Gatehouse -----------------------------
